a6eb64cfe52cbcab6e8ae2732283cc0bd8fb06ae
Naim Mia <nayem20012@gmail.com>
2023-09-15 12:39:59 +0600

    add project

--- bin/age.dart
+++ bin/age.dart
@@ -7,4 +7,24 @@ void main() {
 
   var doubleIt = age(2001);
   print(doubleIt(2023)); // 20
+
+
+  // Home Understanding and make an alternate
+
+  //void main() {
+  // var multiplier = (int x) {
+  //    return (int y) {
+  //      return x * y;
+  //   };
+  // };
+
+  // var doubleIt = multiplier(2);
+  // print(doubleIt(10)); // 20
+  //}
+
+
+
+
+
+
 }
