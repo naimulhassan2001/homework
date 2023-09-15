5a67c760f0c0de217ab3ad1f7f92b189fe539802
Naim Mia <nayem20012@gmail.com>
2023-09-15 16:36:25 +0600

    homework

--- bin/main.dart
+++ bin/main.dart
@@ -1,3 +1,25 @@
-main() {
-  print('Hello world') ;
+import 'dart:io';
+import 'dart:math';
+
+calculation(int loanAmount, int year, int interestRate) {
+  var rate = interestRate / 100;
+
+  var compoundCapital = loanAmount * (pow((1 + rate), year));
+
+  var interest = compoundCapital - loanAmount ;
+
+
+  print("");
+  stdout.write("Loan Amount: $loanAmount  ") ;
+  stdout.write("Year : $year  ") ;
+  stdout.write("Rate : $rate  ") ;
+  stdout.write("interest : $interest  ") ;
+  stdout.write("Compound Capital: $compoundCapital") ;
+
+
+}
+
+void main() {
+  calculation(5000, 2, 12);
+  calculation(5000, 2, 12);
 }
