d12c2b8fe1d4735166a7c2914a6d9eae86c4b0f9
Naim Mia <nayem20012@gmail.com>
2023-09-16 15:55:39 +0600

    homework

--- bin/bankinterest.dart
+++ bin/bankinterest.dart
@@ -1,70 +1,55 @@
 import 'dart:math';
 
+int compoundCallNumber = 1;
+int simpleCallNumber = 1;
+
 compound(int loanAmount, int tenure, int interestRate, Function fn) {
   var rate = interestRate / 100;
   var compoundCapital = loanAmount * (pow((1 + rate), tenure));
-  var interest = compoundCapital - loanAmount ;
-  fn(interest, loanAmount, interestRate, tenure) ;
+  var interest = compoundCapital - loanAmount;
+  fn(interest, loanAmount, interestRate, tenure);
 }
 
-simple(int loanAmount, int year, int interestRate, Function fn ) {
+simple(int loanAmount, int year, int interestRate, Function fn) {
   var rate = interestRate / 100;
-  var interest = loanAmount *rate * year ;
-  fn(interest, loanAmount, interestRate, year ) ;
+  var interest = loanAmount * rate * year;
+  fn(interest, loanAmount, interestRate, year);
 }
-
 
 var compounds = (num interest, num loanAmount, num interestRate, num tenure) {
+  var compoundCapital = loanAmount + interest;
 
-  var compoundCapital = loanAmount+ interest ;
+  if (compoundCallNumber == 1) {
+    print("Compound interest Rate:");
+    print("loan Amount  Tenure  interest Rate  interest Amount     Compound capital");
+    compoundCallNumber += 1;
+  }
 
-
-
-  print("  $loanAmount       $tenure          $interestRate%        $interest   $compoundCapital") ;
-
-
-
-
-} ;
-
+  print("  $loanAmount       $tenure          $interestRate%        $interest   $compoundCapital");
+};
 
 var simples = (num interest, num loanAmount, num interestRate, num tenure) {
   var simpleCapital = interest + loanAmount;
 
-  int number = 0 ;
-
-  if(number == 1) {
-    print("Simple  interest Rate");
-    print('loan Amount   Tenure   Interest Rate  interest Amount  Siple capital');
-    number = number +1  ;
+  if (simpleCallNumber == 1) {
+    print("\nSimple  interest Rate:");
+    print('loan Amount   Tenure   Interest Rate  interest Amount  Simple capital');
+    simpleCallNumber += 1;
   }
 
-
-  print("  $loanAmount        $tenure         $interestRate%              $interest        $simpleCapital");
-
-
+  print("  $loanAmount        $tenure         $interestRate%              $interest           $simpleCapital");
 };
 
+void main() {
+  compound(300000, 2, 4, compounds);
+  compound(500000, 5, 7, compounds);
+  compound(700000, 2, 4, compounds);
+  compound(600000, 4, 6, compounds);
+  compound(900000, 3, 7, compounds);
 
-  void main() {
-
-
-    print("Compound interest Rate") ;
-    print("loan Amount  Tenure  interest Rate  intterest Amount    Compound capital") ;
-    compound(300000, 2, 4, compounds);
-    compound(500000, 5, 7, compounds);
-    compound(700000, 2, 4, compounds);
-    compound(600000, 4, 6, compounds);
-    compound(900000, 3, 7, compounds);
-
-
-  print("\nSimple  interest Rate");
-  print('loan Amount   Tenure   Interest Rate  Totul interest   Siple capital');
-  simple(300000, 4, 4,simples );
-  simple(700000, 2, 5,simples );
-  simple(400000, 4, 6,simples );
-  simple(600000, 3, 5,simples );
-  simple(800000, 2, 5,simples );
-
-
+  simple(300000, 4, 4, simples);
+  simple(700000, 2, 5, simples);
+  simple(400000, 4, 6, simples);
+  simple(600000, 3, 5, simples);
+  simple(800000, 2, 6, simples);
 }
--- bin/main.dart
+++ bin/main.dart
@@ -1,64 +1,59 @@
-import 'dart:io';
 import 'dart:math';
+
+int compoundCallNumber = 1;
+int simpleCallNumber = 1;
 
 compound(int loanAmount, int tenure, int interestRate, Function fn) {
   var rate = interestRate / 100;
   var compoundCapital = loanAmount * (pow((1 + rate), tenure));
-  var interest = compoundCapital - loanAmount ;
-  fn(interest, loanAmount, interestRate, tenure) ;
+  var interest = compoundCapital - loanAmount;
+  fn(interest, loanAmount, interestRate, tenure);
 }
 
-simple(int loanAmount, int year, int interestRate, Function fn ) {
+simple(int loanAmount, int year, int interestRate, Function fn) {
   var rate = interestRate / 100;
-  var interest = loanAmount *rate * year ;
-  fn(interest, loanAmount, interestRate, year ) ;
+  var interest = loanAmount * rate * year;
+  fn(interest, loanAmount, interestRate, year);
 }
 
+var compounds = (num interest, num loanAmount, num interestRate, num tenure) {
+  var compoundCapital = loanAmount + interest;
+
+  if (compoundCallNumber == 1) {
+    print("Compound interest Rate:");
+    print(
+        "loan Amount  Tenure  interest Rate  interest Amount     Compound capital");
+    compoundCallNumber += 1;
+  }
+
+  print(
+      "  $loanAmount       $tenure          $interestRate%        $interest   $compoundCapital");
+};
+
+var simples = (num interest, num loanAmount, num interestRate, num tenure) {
+  var simpleCapital = interest + loanAmount;
+
+  if (simpleCallNumber == 1) {
+    print("\nSimple  interest Rate:");
+    print(
+        'loan Amount   Tenure   Interest Rate  interest Amount  Simple capital');
+    simpleCallNumber += 1;
+  }
+
+  print(
+      "  $loanAmount        $tenure         $interestRate%              $interest         $simpleCapital");
+};
 
 void main() {
-  compound(100000, 3, 5, (num interest, num loanAmount, num interestRate, num tenure) {
+  compound(300000, 2, 4, compounds);
+  compound(500000, 5, 7, compounds);
+  compound(700000, 2, 4, compounds);
+  compound(600000, 4, 6, compounds);
+  compound(900000, 3, 7, compounds);
 
-    var compoundCapital = loanAmount+ interest ;
-
-    print("Compound interest Rate") ;
-
-    print("loan Amount: $loanAmount  Tenure: $tenure  interest Rate: $interestRate  intterest Amount: $interest  simple capital: $compoundCapital") ;
-
-    print("-------------------------------------------------------------------------------------") ;
-    // stdout.write("Loan Amount: $loanAmount") ;
-    // stdout.write(' | ') ;
-    // stdout.write("Tenure : $tenure") ;
-    // stdout.write(' | ') ;
-    // stdout.write(" interest Rate : $interestRate") ;
-    // stdout.write(' | ') ;
-    // stdout.write("interest Amount: $interest") ;
-    // stdout.write(' | ') ;
-    // stdout.write("Compound Capital: ${loanAmount+interest}\n");
-
-
-
-
-  });
-
-  simple(100000, 3, 5, (num interest, num loanAmount, num interestRate, num tenure) {
-
-    var simpleCapital = interest + loanAmount ;
-
-    print("Simple  interest Rate");
-
-    print("loan Amount: $loanAmount  Tenure: $tenure  interest Rate: $interestRate interest Amount: $interest  siple capital: $simpleCapital") ;
-    print("---------------------------------------------------------------------------------------") ;
-    // stdout.write("Loan Amount: $loanAmount") ;
-    // stdout.write(' | ') ;
-    // stdout.write("Tenure : $tenure") ;
-    // stdout.write(' | ') ;
-    // stdout.write(" interest Rate : $interestRate") ;
-    // stdout.write(' | ') ;
-    // stdout.write("interest Amount: $interest") ;
-    // stdout.write(' | ') ;
-    // stdout.write(" simple Capital: $simpleCapital\n");
-
-
-  });
-
+  simple(300000, 4, 4, simples);
+  simple(700000, 2, 5, simples);
+  simple(400000, 4, 6, simples);
+  simple(600000, 3, 5, simples);
+  simple(800000, 2, 6, simples);
 }
