4ea828e97407f2a99bccf946b0e32efd543e3151
Naim Mia <nayem20012@gmail.com>
2023-09-16 16:09:43 +0600

    homework

--- bin/bankinterest.dart
+++ bin/bankinterest.dart
@@ -16,7 +16,7 @@ simple(int loanAmount, int year, int interestRate, Function fn) {
   fn(interest, loanAmount, interestRate, year);
 }
 
-var compounds = (num interest, num loanAmount, num interestRate, num tenure) {
+var compoundInterest = (num interest, num loanAmount, num interestRate, num tenure) {
   var compoundCapital = loanAmount + interest;
 
   if (compoundCallNumber == 1) {
@@ -28,7 +28,7 @@ var compounds = (num interest, num loanAmount, num interestRate, num tenure) {
   print("  $loanAmount       $tenure          $interestRate%        $interest   $compoundCapital");
 };
 
-var simples = (num interest, num loanAmount, num interestRate, num tenure) {
+var simpleInterest = (num interest, num loanAmount, num interestRate, num tenure) {
   var simpleCapital = interest + loanAmount;
 
   if (simpleCallNumber == 1) {
@@ -41,15 +41,15 @@ var simples = (num interest, num loanAmount, num interestRate, num tenure) {
 };
 
 void main() {
-  compound(300000, 2, 4, compounds);
-  compound(500000, 5, 7, compounds);
-  compound(700000, 2, 4, compounds);
-  compound(600000, 4, 6, compounds);
-  compound(900000, 3, 7, compounds);
+  compound(300000, 2, 4, compoundInterest);
+  compound(500000, 5, 7, compoundInterest);
+  compound(700000, 2, 4, compoundInterest);
+  compound(600000, 4, 6, compoundInterest);
+  compound(900000, 3, 7, compoundInterest);
 
-  simple(300000, 4, 4, simples);
-  simple(700000, 2, 5, simples);
-  simple(400000, 4, 6, simples);
-  simple(600000, 3, 5, simples);
-  simple(800000, 2, 6, simples);
+  simple(300000, 4, 4, simpleInterest);
+  simple(700000, 2, 5, simpleInterest);
+  simple(400000, 4, 6, simpleInterest);
+  simple(600000, 3, 5, simpleInterest);
+  simple(800000, 2, 6, simpleInterest);
 }
