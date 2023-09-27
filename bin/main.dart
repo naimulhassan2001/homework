ab3b754e25c578a47607499405bf3bcf1b60ed67
Naim Mia <nayem20012@gmail.com>
2023-09-27 23:56:56 +0600

    homework

--- bin/main.dart
+++ bin/main.dart
@@ -1,13 +1,23 @@
-class Point extends Object {
-  int x = 0;
-  int y = 0;
-  Point(this.x, this.y);
-  @override
-  String toString() {
-    return "x=$x, y=$y ${DateTime.now()}";
-  }
+class Book {
+  String title;
+  int price = 0;
+  Book({this.title = '', this.price = 0});
 }
+
 void main() {
-  Point point = Point(10, 20);
-  print(point.toString());
+  var people = [
+    Book(title: 'book1', price: 100),
+    Book(title: 'book2', price: 200),
+    Book(title: 'book3', price: 300),
+    Book(title: 'book4', price: 400),
+    Book(title: 'book5', price: 500),
+  ];
+  List list = [];
+  for (var person in people) {
+    list.add(person.price);
+  }
+
+  var sum = list.reduce((v, e) => v + e);
+
+  print(sum);
 }
